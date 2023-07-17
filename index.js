5487cc30604abd16ab19db16453ed3515884eb5d
Abanum Olise <abanum00@gmail.com>
2023-07-17 02:45:51 +0100

    mobile clean up

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let accordions= document.querySelectorAll(".accordion-header");
+let accordions = document.querySelectorAll(".accordion-header");
 
-for(let accordion of accordions) {
+for (let accordion of accordions) {
     let accordionTitle = accordion.children[0];
     let accordionIcon = accordion.children[1];
     let accordionPanel = accordion.nextElementSibling;
-    accordion.addEventListener("click", ()=>{
+
+    accordion.addEventListener("click", () => {       
         accordionTitle.classList.toggle("selected");
         accordionIcon.classList.toggle("selected");
         accordionPanel.classList.toggle("selected");
