a3494ba0e1ea9cb6ce713c6909f52c1dac54facd
Abanum Olise <abanum00@gmail.com>
2023-07-17 03:42:28 +0100

    clean up

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ let accordions = document.querySelectorAll(".accordion-header");
 for (let accordion of accordions) {
     let accordionTitle = accordion.children[0];
     let accordionIcon = accordion.children[1];
-    let accordionPanel = accordion.nextElementSibling;
+    let accordionPanel = accordion.nextElementSibling;    
 
-    accordion.addEventListener("click", () => {       
+    accordion.addEventListener("click", () => { 
+        let panelHeight = accordionPanel.style.maxHeight; 
+        let panelHeightNum = panelHeight.slice(0, panelHeight.length - 2);
+
         accordionTitle.classList.toggle("selected");
-        accordionIcon.classList.toggle("selected");
-        accordionPanel.classList.toggle("selected");
+        accordionIcon.classList.toggle("selected");        
+        
+        if(panelHeightNum){  
+            accordionPanel.style.maxHeight = null;  
+            return
+        }
+        accordionPanel.style.maxHeight = `${accordionPanel.scrollHeight}px`;        
     })
 }
