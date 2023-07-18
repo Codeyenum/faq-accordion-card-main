6d75dd41d2d4d1605c04c7d3780b10d80951f443
Abanum Olise <abanum00@gmail.com>
2023-07-18 14:28:50 +0100

    accordion func clean up

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 let accordions = document.querySelectorAll(".accordion-header");
 
+// loop through the accordion elements to be clicked
 for (let accordion of accordions) {
     let accordionTitle = accordion.children[0];
     let accordionIcon = accordion.children[1];
-    let accordionPanel = accordion.nextElementSibling;    
+    let accordionPanel = accordion.nextElementSibling;
 
-    accordion.addEventListener("click", () => { 
-        let panelHeight = accordionPanel.style.maxHeight; 
-        let panelHeightNum = panelHeight.slice(0, panelHeight.length - 2);
-
-        accordionTitle.classList.toggle("selected");
-        accordionIcon.classList.toggle("selected");        
-        
-        if(panelHeightNum){  
-            accordionPanel.style.maxHeight = null;  
+    // set click event listener on each accordion element
+    accordion.addEventListener("click", (e) => {
+        // checks if the clicked element is already active
+        if (e.currentTarget.classList.contains("active")) {
+            // removes active styling and active status            
+            accordionTitle.classList.remove("selected");
+            accordionIcon.classList.remove("selected");
+            accordionPanel.style.maxHeight = null;
+            accordion.classList.remove("active");
             return
         }
-        accordionPanel.style.maxHeight = `${accordionPanel.scrollHeight}px`;        
+        // removes active styling and status for inactive elements
+        for (let accordion of accordions) {
+            let accordionTitle = accordion.children[0];
+            let accordionPanel = accordion.nextElementSibling;
+            let accordionIcon = accordion.children[1];
+
+            accordionTitle.classList.remove("selected");
+            accordionIcon.classList.remove("selected");
+            accordionPanel.style.maxHeight = null;
+            accordion.classList.remove("active");
+        }
+        // adds active styling and status for clicked element
+        accordionTitle.classList.add("selected");
+        accordionIcon.classList.add("selected");
+        accordionPanel.style.maxHeight = `${accordionPanel.scrollHeight}px`;
+        accordion.classList.add("active");
     })
 }
